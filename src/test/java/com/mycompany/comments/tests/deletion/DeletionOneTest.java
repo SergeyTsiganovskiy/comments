c1b0b6001c022ed9a6004269534e806d4111cf99
tsyganovskiy <tsyganovskiy.s@mail.ru>
2017-06-15 22:59:01 +0300

    Created deletion comments tests

--- src/test/java/com/mycompany/comments/tests/deletion/DeletionOneTest.java
+++ src/test/java/com/mycompany/comments/tests/deletion/DeletionOneTest.java
@@ -1,4 +1,23 @@
 package com.mycompany.comments.tests.deletion;
 
-public class DeletionOneTest {
+import com.mycompany.comments.pages.DeleteConfirmationPopup;
+import com.mycompany.comments.pages.IndexPage;
+import com.mycompany.comments.pages.Table;
+import com.mycompany.comments.tests.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DeletionOneTest extends BaseTest {
+
+    @Test
+    public void testDeleteOneComment() {
+        IndexPage indexPage = new IndexPage();
+        Table table = new Table();
+        String NUMBER_OF_DELETED_COMMENT = table.getTableRows().get(1).getCommentNumber().getText();
+        table.getTableRows().get(1).getCheckbox().click();
+        indexPage.clickDeleteLink();
+        new DeleteConfirmationPopup().clickYes();
+        Assert.assertFalse(table.isCommentOnPage(NUMBER_OF_DELETED_COMMENT));
+    }
 }
+
--- src/test/java/com/mycompany/comments/tests/deletion/DeletionSomeTest.java
+++ src/test/java/com/mycompany/comments/tests/deletion/DeletionSomeTest.java
@@ -1,4 +1,25 @@
 package com.mycompany.comments.tests.deletion;
 
-public class DeletionSomeTest {
+import com.mycompany.comments.pages.DeleteConfirmationPopup;
+import com.mycompany.comments.pages.IndexPage;
+import com.mycompany.comments.pages.Table;
+import com.mycompany.comments.tests.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DeletionSomeTest extends BaseTest {
+
+    @Test
+    public void testDeleteSomeComment() {
+        IndexPage indexPage = new IndexPage();
+        Table table = new Table();
+        String FIRST_NUMBER_OF_DELETED_COMMENT = table.getTableRows().get(1).getCommentNumber().getText();
+        String SECOND_NUMBER_OF_DELETED_COMMENT = table.getTableRows().get(2).getCommentNumber().getText();
+        table.getTableRows().get(1).getCheckbox().click();
+        table.getTableRows().get(2).getCheckbox().click();
+        indexPage.clickDeleteLink();
+        new DeleteConfirmationPopup().clickYes();
+        Assert.assertFalse(table.isCommentOnPage(FIRST_NUMBER_OF_DELETED_COMMENT));
+        Assert.assertFalse(table.isCommentOnPage(SECOND_NUMBER_OF_DELETED_COMMENT));
+    }
 }
